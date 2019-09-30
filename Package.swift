23c8410db7e1a3d3159313ecae8f1910ebe1dd0f
Dillon McElhinney <dillon.mce@gmail.com>
2019-09-29 20:40:48 -0600

    Add path for targets

--- Package.swift
+++ Package.swift
@@ -21,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CardSwipe",
-            dependencies: []),
+            dependencies: [],
+            path: "CardSwipe"),
         .testTarget(
             name: "CardSwipeTests",
-            dependencies: ["CardSwipe"]),
+            dependencies: ["CardSwipe"],
+            path: "CardSwipe"),
     ]
 )
